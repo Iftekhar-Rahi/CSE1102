b3ff8c739291275e15d349e65d025ca2f335841d
Iftekhar-Rahi <iftekharhossainrahi.gmail.com>
2024-01-14 23:13:13 +0600

    Commit

--- 1. Lab Assignment/Lab Assignment 4/Answers/Task 5.py
+++ 1. Lab Assignment/Lab Assignment 4/Answers/Task 5.py
@@ -1,4 +1,5 @@
-word=input('Enter a string: ')
-for i in range (1,len(word)+1):
-  word2=word[ :i: ]
-  print(word2)
+word=input()
+for i in range(1,len(word)+1):
+  for j in range(0,i):
+    print(word[j],end="")
+  print()
